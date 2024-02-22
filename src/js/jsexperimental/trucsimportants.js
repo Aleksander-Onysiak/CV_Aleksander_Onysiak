ca8d4e9031ae05a89d4ec53c96a79d1979cf702a
Aleksander-Onysiak <onysaleks@gmail.com>
2024-02-22 15:28:51 +0100

    CV update

---
+++ src/js/jsexperimental/trucsimportants.js
@@ -0,0 +1,222 @@
+<script type="application/ld+json" data-gatsby-head="true">
+    {
+        "@context": "http://schema.org",
+        "@type": "BreadcrumbList",
+        "description": "Breadcrumbs list",
+        "name": "Breadcrumbs",
+        "itemListElement": [{
+        "@type": "ListItem",
+        "item": {
+        "@id": "https://www.wooclap.com/",
+        "name": "homepage"
+    },
+        "position": 1
+    }]
+    }
+</script>
+<script type="application/ld+json" data-gatsby-head="true">
+    {
+        "@context": "http://schema.org",
+        "@type": "WebPage",
+        "url": "https://www.wooclap.com/",
+        "headline": "Create Interactive Presentations with Wooclap and engage Your Audience",
+        "inLanguage": "en",
+        "mainEntityOfPage": "https://www.wooclap.com/",
+        "description": "Engage your audience with Wooclap's interactive presentation tool. Create dynamic and engaging presentations that keep your audience hooked. Try Wooclap today and take your presentations to the next level.",
+        "name": "Create Interactive Presentations with Wooclap and engage Your Audience",
+        "author": {
+        "@type": "Person",
+        "name": "Wooclap"
+    },
+        "copyrightHolder": {
+        "@type": "Person",
+        "name": "Wooclap"
+    },
+        "copyrightYear": "2021",
+        "creator": {
+        "@type": "Person",
+        "name": "Wooclap"
+    },
+        "publisher": {
+        "@type": "Person",
+        "name": "Wooclap"
+    },
+        "datePublished": "2021-07-09T09:30:00+01:00",
+        "dateModified": "2024-01-24",
+        "image": {
+        "@type": "ImageObject",
+        "url": "https://images.prismic.io/wooclap-content/bdc3a3f9-e7b3-429e-9c3f-7f5fb3152a50_Logo-Square.png?ixlib=gatsbyFP&auto=compress%2Cformat&fit=max&rect=0%2C238%2C1000%2C523&w=1200&h=627"
+    }
+    }
+</script>
+<script type="application/ld+json" data-gatsby-head="true">
+    {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": "Create Interactive Presentations with Wooclap and engage Your Audience",
+        "url": "https://www.wooclap.com/"
+    }
+</script>
+<script type="application/ld+json" data-gatsby-head="true">
+    {
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "url": "https://www.wooclap.com/",
+        "logo": "https://www.wooclap.com/images/logo/logo-blue.png",
+        "sameAs": ["https://www.facebook.com/wooclap", "https://twitter.com/wooclap", "https://www.linkedin.com/company/wooclapcompany/"],
+        "address": {
+        "@type": "PostalAddress",
+        "addressCountry": "BE",
+        "addressLocality": "Brussels",
+        "postalCode": "1040",
+        "streetAddress": "Rue des Pères Blancs, 4"
+    },
+        "contactPoint": {
+        "@type": "ContactPoint",
+        "email": "info@wooclap.com",
+        "contactType": "customer service",
+        "url": "https://docs.wooclap.com/en/"
+    },
+        "email": "info@wooclap.com",
+        "founder": [{
+        "@type": "Person",
+        "givenName": "Sébastien",
+        "familyName": "Lebbe",
+        "jobTitle": "CEO"
+    }, {
+        "@type": "Person",
+        "givenName": "Jonathan",
+        "familyName": "Alzetta",
+        "jobTitle": "CTO"
+    }, {
+        "@type": "Person",
+        "givenName": "Olivier",
+        "familyName": "Verdin",
+        "jobTitle": "Co-founder"
+    }],
+        "legalName": "Wooclap SA",
+        "vatID": "BE0563.691.645"
+    }
+</script>
+
+<script type="module">
+    const e = "undefined" != typeof HTMLImageElement && "loading" in HTMLImageElement.prototype;
+    e && document.body.addEventListener("load", (function (e) {
+    const t = e.target;
+    if (void 0 === t.dataset.mainImage) return;
+    if (void 0 === t.dataset.gatsbyImageSsr) return;
+    let a = null,
+    n = t;
+    for (; null === a && n;) void 0 !== n.parentNode.dataset.gatsbyImageWrapper && (a = n.parentNode), n = n.parentNode;
+    const o = a.querySelector("[data-placeholder-image]"),
+    r = new Image;
+    r.src = t.currentSrc, r.decode().catch((() => {})).then((() => {
+    t.style.opacity = 1, o && (o.style.opacity = 0, o.style.transition = "opacity 500ms linear")
+}))
+}), !0
+    )
+    ;
+</script>
+
+
+<script type="text/javascript" id="sendinblue-js" async=""
+        src="https://sibautomation.com/sa.js?key=0k4si4f0k4ej6t0h0aa0j"></script>
+<script type="text/javascript" async="" src="https://snap.licdn.com/li.lms-analytics/insight.min.js"></script>
+<script src="" async=""></script>
+<script type="text/javascript" async="" src="https://connect.facebook.net/en_US/fbevents.js"></script>
+<script type="text/javascript" async="" src="https://static.hotjar.com/c/hotjar-141056.js?sv=7"></script>
+<script type="text/javascript" async=""
+        src="https://www.googletagmanager.com/gtag/js?id=G-96SN7LSQGV&amp;l=dataLayer&amp;cx=c"></script>
+<script type="text/javascript" async=""
+        src="https://www.googletagmanager.com/gtag/destination?id=G-96SN7LSQGV&amp;l=dataLayer&amp;cx=c"></script>
+<script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-TGGLGM6"></script>
+
+
+••••••••••••••••••••••••••••
+
+••••••••••••••••••••••••••••
+<style data-styled="active" data-styled-version="5.3.9"></style>
+<link rel="manifest" href="/manifest.webmanifest" crossorigin="anonymous"/>
+<link rel="apple-touch-icon" sizes="36x36" href="/assets/images/android-icon-36x36.png"/>
+<link rel="apple-touch-icon" sizes="48x48" href="/assets/images/android-icon-48x48.png"/>
+<link rel="apple-touch-icon" sizes="72x72" href="/assets/images/android-icon-72x72.png"/>
+<link rel="apple-touch-icon" sizes="96x96" href="/assets/images/android-icon-96x96.png"/>
+<link rel="sitemap" type="application/xml" href="/sitemap-index.xml"/>
+<title data-gatsby-head="true">Create Interactive Presentations with Wooclap and engage Your Audience</title>
+<link href="https://www.wooclap.com/" rel="canonical" data-gatsby-head="true"/>
+<link href="https://www.wooclap.com/" hreflang="x-default" rel="alternate" data-gatsby-head="true"/>
+<link href="https://www.wooclap.com/" hreflang="en" rel="alternate" data-gatsby-head="true"/>
+<link href="https://www.wooclap.com/de/" hreflang="de" rel="alternate" data-gatsby-head="true"/>
+<link href="https://www.wooclap.com/nl/" hreflang="nl" rel="alternate" data-gatsby-head="true"/>
+<link href="https://www.wooclap.com/es/" hreflang="es" rel="alternate" data-gatsby-head="true"/>
+<link href="https://www.wooclap.com/fr/" hreflang="fr" rel="alternate" data-gatsby-head="true"/>
+<link href="https://www.wooclap.com/it/" hreflang="it" rel="alternate" data-gatsby-head="true"/>
+<link href="" rel="apple-touch-icon" sizes="57x57" type="image/avif" data-gatsby-head="true"/>
+<link href="" rel="apple-touch-icon" sizes="60x60" type="image/avif" data-gatsby-head="true"/>
+<link href="" rel="apple-touch-icon" sizes="72x72" type="image/avif" data-gatsby-head="true"/>
+<link href="" rel="apple-touch-icon" sizes="114x114" type="image/avif" data-gatsby-head="true"/>
+<link href="" rel="apple-touch-icon" sizes="120x120" type="image/avif" data-gatsby-head="true"/>
+<link href="sizes=" x144="type=&quot;" image="" avif="data-gatsby-head=&quot;" true=""/> &lt; link
+href = ""
+rel = "apple-touch-icon"
+sizes = "152x152"
+type = "image/avif"
+data - gatsby - head = "true" &gt; / &gt; &lt; link
+href = ""
+rel = "apple-touch-icon"
+sizes = "180x180"
+type = "image/avif"
+data - gatsby - head = "true" &gt; / &gt; &lt; link
+href = ""
+rel = "icon"
+sizes = "48x48"
+type = "image/avif"
+data - gatsby - head = "true" &gt; / &gt; &lt; link
+href = ""
+rel = "icon"
+sizes = "72x72"
+type = "image/avif"
+data - gatsby - head = "true" &gt; / &gt; &lt; link
+href = ""
+rel = "icon"
+sizes = "96x96"
+type = "image/avif"
+data - gatsby - head = "true" &gt; / &gt; &lt; link
+href = ""
+rel = "icon"
+sizes = "144x144"
+type = "image/avif"
+data - gatsby - head = "true" &gt; / &gt; &lt; link
+href = ""
+rel = "icon"
+sizes = "192x192"
+type = "image/avif"
+data - gatsby - head = "true" &gt; / &gt; &lt; link
+href = ""
+rel = "icon"
+sizes = "16x16"
+type = "image/avif"
+data - gatsby - head = "true" &gt; / &gt; &lt; link
+href = ""
+rel = "icon"
+sizes = "32x32"
+type = "image/avif"
+data - gatsby - head = "true" &gt; / &gt; &lt; link
+href = ""
+rel = "icon"
+sizes = "96x96"
+type = "image/avif"
+data - gatsby - head = "true" &gt; / &gt; &lt; link
+rel = "preconnect"
+href = "https://dev.visualwebsiteoptimizer.com"
+data - gatsby - head = "true" &gt; / &gt; &lt; script
+src = "https://dev.visualwebsiteoptimizer.com/j.php?a=743018&amp;u=https%3A%2F%2Fwww.wooclap.com%2F&amp;vn=2"
+type = "text/src" &gt; &lt; /script&gt;
+<link rel="preconnect" href="https://static.cdn.prismic.io"/>
+/&gt;
+<link rel="dns-prefetch" href="https://static.cdn.prismic.io"/>
+/&gt;
+<meta http-equiv="origin-trial"
+      content="AymqwRC7u88Y4JPvfIF2F37QKylC04248hLCdJAsh8xgOfe/dVJPV3XS3wLFca1ZMVOtnBfVjaCMTVudWM//5g4AAAB7eyJvcmlnaW4iOiJodHRwczovL3d3dy5nb29nbGV0YWdtYW5hZ2VyLmNvbTo0NDMiLCJmZWF0dXJlIjoiUHJpdmFjeVNhbmRib3hBZHNBUElzIiwiZXhwaXJ5IjoxNjk1MTY3OTk5LCJpc1RoaXJkUGFydHkiOnRydWV9"/>
+/&gt;&gt;
+<script async src="https://script.hotjar.com/modules.0c2aac1b2d1ba79f2a01.js" charset="utf-8"></script>
